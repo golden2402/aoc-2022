9706dc2695d61a1eb73380cecd437b7a8502914b
golden2402 <golden2402@vt.edu>
2022-12-04 16:50:20 -0500

    formatting

--- day3/index.py
+++ day3/index.py
@@ -15,4 +15,5 @@ print(sum([to_priority(list(set(rucksack[:(len(rucksack) // 2)]) &
       set(rucksack[(len(rucksack) // 2):]))[0]) for rucksack in lines]))
 
 # part 2:
-print(sum([to_priority(list(set(chunk[0]) & set(chunk[1]) & set(chunk[2]))[0]) for chunk in [lines[n:n+3] for n in range(0, len(lines), 3)]]))
+print(sum([to_priority(list(set(chunk[0]) & set(chunk[1]) & set(chunk[2]))[0])
+      for chunk in [lines[n:n+3] for n in range(0, len(lines), 3)]]))
--- day4/index.py
+++ day4/index.py
@@ -1,13 +1,19 @@
 import sys
 import re
 
+
 def contained(x1, x2, y1, y2):
     return x1 >= y1 and x2 <= y2 or x1 <= y1 and x2 >= y2
+
 
 def intersect(x1, x2, y1, y2):
     return x2 >= y1 and x1 <= y2
 
-groups = [[int(field) for field in re.split("-|,", line.rstrip('\n'))] for line in sys.stdin]
 
+groups = [[int(field) for field in re.split("-|,", line.rstrip())]
+          for line in sys.stdin]
+
+# part 1:
 print(sum([1 for group in groups if contained(*group)]))
+# part 2:
 print(sum([1 for group in groups if intersect(*group)]))
