2d90da522035c3b9cb8fb3e1881de8e31e6888fd
golden2402 <golden2402@vt.edu>
2022-12-04 16:50:03 -0500

    change py to use stdin

--- day1/using_lists.py
+++ day1/using_lists.py
@@ -1,3 +1,5 @@
-with open("./sample.txt") as f:
-    elves = sorted([sum([int(line) for line in group.split('\n')]) for group in f.read().split("\n\n")])
-    print(elves[-1], sum(elves[-3:]))
+import sys
+
+elves = sorted([sum([int(line) for line in group.split('\n')])
+               for group in sys.stdin.read().split("\n\n")])
+print(elves[-1], sum(elves[-3:]))
--- day1/verbose.py
+++ day1/verbose.py
@@ -3,25 +3,24 @@ def main():
     elves = [0]
     elf_i = 0
 
-    with open("./sample.txt", "r") as file_data:
-        for line in file_data:
-            if len(line) > 1:
-                elves[elf_i] += int(line)
-            else:
-                elves.append(0)
-                elf_i += 1
+    for line in sys.stdin:
+        if len(line) > 1:
+            elves[elf_i] += int(line)
+        else:
+            elves.append(0)
+            elf_i += 1
+
+    elves = sorted(elves)
+    elves_count = len(elves)
+
+    # part 1:
+    print("Most calories:", elves[elves_count - 1])
+
+    calorie_sum = 0
+    for i in range(-3, 0):
+        calorie_sum += elves[i]
     
-        elves = sorted(elves)
-        elves_count = len(elves)
-
-        # part 1:
-        print("Most calories:", elves[elves_count - 1])
-
-        calorie_sum = 0
-        for i in range(-3, 0):
-            calorie_sum += elves[i]
-        
-        print("Top 3 total calories:", calorie_sum)
+    print("Top 3 total calories:", calorie_sum)
 
 
 if __name__ == "__main__":
